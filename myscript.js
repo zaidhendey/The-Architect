ae1f37392e6b908282d21d9cb439947647ab084c
zaidhendey <zaid.hendey@yahoo.com>
2020-08-26 01:29:49 +0300

    this is my last day in the heaven

---
+++ myscript.js
@@ -0,0 +1,37 @@
+var name ='Zaid Hendey';
+var age=30;
+var homeadress ="Hama syria";
+var gender;
+var day=new Date();
+var true1=true;
+console.log('the name',name);
+console.log('the age',age);
+console.log('the home adress',homeadress);
+console.log('the gender',gender);
+console.log('day',day);
+console.log('true1',true1);
+
+//console.log(' type of name ',typeof name);
+//console.log(' type of age ',typeof age);
+/*
+
+console.log(' type of homeadress ',typeof homeadress);
+console.log(' type of day ',typeof day);
+console.log(' type of true1 ',typeof true1);
+console.log(' type of gender ',typeof gender);
+*/
+
+
+if(age==30 &&name=='Zaid Hendey'){
+    alert('you are welcome');
+}else{
+    console.log(' stay here ');
+}
+
+//prompt('what is going on');
+var favmov;
+favmov = prompt(' what is your fav movie : ');
+console.log('user\'s fav mov', favmov);
+
+var favAct = confirm('nice one');
+console.log('user fav actress' ,favAct);
